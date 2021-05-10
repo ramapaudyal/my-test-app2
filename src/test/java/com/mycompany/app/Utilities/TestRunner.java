57a9d1113bad98d6bb1c2fad3fc8b746fb5276b6
ramapaudyal <rama.paudyal@cyphertree.com>
2021-05-10 10:43:52 +0100

    Update TestRunner.java
    comments

--- src/test/java/com/mycompany/app/Utilities/TestRunner.java
+++ src/test/java/com/mycompany/app/Utilities/TestRunner.java
@@ -3,6 +3,7 @@ package com.mycompany.app.Utilities;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
+/* class is empty but the annotations do the job */
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = {" . "}, glue = {"com.mycompany.app.StepDefinitions"})
